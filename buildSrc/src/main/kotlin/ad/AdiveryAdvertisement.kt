50dba9ab218e27d4a80ba9c1e83fd8711debac6d
ali tafreshi <alitafreshi123@yahoo.com>
2022-06-25 10:41:05 +0430

    update adivery to 4.3.6

--- buildSrc/src/main/kotlin/ad/AdiveryAdvertisement.kt
+++ buildSrc/src/main/kotlin/ad/AdiveryAdvertisement.kt
@@ -1,6 +1,6 @@
 package ad
 
 object AdiveryAdvertisement {
-    private const val adVersion = "0.1.0"
+    private const val adVersion = "0.1.2"
     const val advertisement = "com.github.sahar-hoseinkhani:advertisement:$adVersion"
 }
