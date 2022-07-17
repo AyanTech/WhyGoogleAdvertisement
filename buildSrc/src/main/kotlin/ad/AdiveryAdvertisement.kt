a54c74a78037b95fd99caf5772c1d9473ffb5a14
ali tafreshi <alitafreshi123@yahoo.com>
2022-07-17 08:57:51 +0430

    Done

--- buildSrc/src/main/kotlin/ad/AdiveryAdvertisement.kt
+++ buildSrc/src/main/kotlin/ad/AdiveryAdvertisement.kt
@@ -1,6 +1,6 @@
 package ad
 
 object AdiveryAdvertisement {
-    private const val adVersion = "0.1.2"
+    private const val adVersion = "0.1.3"
     const val advertisement = "com.github.sahar-hoseinkhani:advertisement:$adVersion"
 }
