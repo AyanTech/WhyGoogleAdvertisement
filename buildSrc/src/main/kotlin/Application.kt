713d1d98410fa5e3b4fc59fd4fa98b6ca6fe74e0
ali tafreshi <alitafreshi123@yahoo.com>
2022-08-21 17:46:52 +0430

    Adding Admob Proguard Rules

--- buildSrc/src/main/kotlin/Application.kt
+++ buildSrc/src/main/kotlin/Application.kt
@@ -2,7 +2,7 @@ object Application {
     const val appId = "ir.tafreshiali.whygoogleadvertisement"
     const val versionCode = 1
     const val versionName = "1.0.0"
-    const val minSdk = 19
-    const val compileSdk = 31
-    const val targetSdk = 31
+    const val minSdk = 21
+    const val compileSdk = 32
+    const val targetSdk = 32
 }
