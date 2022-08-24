578bfaf437b77ce295eddbfe7334302da65c7651
ali tafreshi <alitafreshi123@yahoo.com>
2022-08-24 15:47:02 +0430

    Done

--- whyoogle-ads/src/main/java/ir/tafreshiali/whyoogle_ads/AdvertisementCore.kt
+++ whyoogle-ads/src/main/java/ir/tafreshiali/whyoogle_ads/AdvertisementCore.kt
@@ -1,14 +1,29 @@
 package ir.tafreshiali.whyoogle_ads
 
 
+import android.app.Application
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.nativead.NativeAd
+import ir.tafreshiali.whyoogle_ads.admob.AdmobAdvertisement
+import ir.tafreshiali.whyoogle_ads.admob.AdmobAdvertisementImpl
 
 object AdvertisementCore {
 
     var admobInterstitialAdvertisement: InterstitialAd? = null
+
     var admobNativeAdvertisement: NativeAd? = null
 
+    lateinit var admobAdvertisement: AdmobAdvertisement
+
+    lateinit var ayanAdvertisement: AyanAdvertisement
+
+    fun init(application: Application) {
+        admobAdvertisement = AdmobAdvertisementImpl()
+        ayanAdvertisement = AyanAdvertisementImpl(
+            context = application,
+            admobAdvertisement = admobAdvertisement
+        )
+    }
 
     fun updateApplicationAdmobInterstitialAdvertisement(admobInterstitialAd: InterstitialAd?) {
         admobInterstitialAdvertisement = admobInterstitialAd
@@ -16,6 +31,6 @@ object AdvertisementCore {
 
     fun updateApplicationAdmobNativeAdvertisement(admobNativeAd: NativeAd? = null) {
         admobNativeAdvertisement = admobNativeAd
-        
+
     }
 }
