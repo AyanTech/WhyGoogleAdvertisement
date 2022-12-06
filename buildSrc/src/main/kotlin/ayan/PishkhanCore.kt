fc32ed25d0c8666f5ba0ab2785a7892bb2d0a620
ali tafreshi <alitafreshi123@yahoo.com>
2022-12-06 11:13:13 +0330

    Updating Dependencies

--- buildSrc/src/main/kotlin/ayan/PishkhanCore.kt
+++ buildSrc/src/main/kotlin/ayan/PishkhanCore.kt
@@ -1,6 +1,6 @@
 package ayan
 
 object PishkhanCore {
-    private const val pishkhanCoreVersion = "0.2.27"
+    private const val pishkhanCoreVersion = "1.0.3"
     const val pishkhanCore = "com.github.sahar-hoseinkhani:ayan:$pishkhanCoreVersion"
 }
