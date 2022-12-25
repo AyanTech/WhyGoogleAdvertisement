090c15ab209ec06704c3e46b5057a72e1019f306
ali tafreshi <alitafreshi123@yahoo.com>
2022-12-25 12:27:41 +0330

    Adding Outline Button Style

--- whyoogle-ads/build.gradle.kts
+++ whyoogle-ads/build.gradle.kts
@@ -99,7 +99,7 @@ afterEvaluate {
                 artifactId = "whygoogle-ads"
 
                 // Version Library Name (Example : "1.0.0")
-                version = "1.3.4"
+                version = "1.3.5"
             }
         }
     }
