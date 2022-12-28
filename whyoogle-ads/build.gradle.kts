eaa2e425d31347b762b99b7d7d34959b8c64d5ed
ali tafreshi <alitafreshi123@yahoo.com>
2022-12-28 15:59:50 +0330

    Done

--- whyoogle-ads/build.gradle.kts
+++ whyoogle-ads/build.gradle.kts
@@ -99,7 +99,7 @@ afterEvaluate {
                 artifactId = "whygoogle-ads"
 
                 // Version Library Name (Example : "1.0.0")
-                version = "1.4.1"
+                version = "1.4.2"
             }
         }
     }
