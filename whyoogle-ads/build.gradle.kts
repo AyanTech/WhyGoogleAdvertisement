59b27500445abe4987a131c554a29e0360585097
ali tafreshi <alitafreshi123@yahoo.com>
2023-01-07 08:59:20 +0330

    Renaming drawable files

--- whyoogle-ads/build.gradle.kts
+++ whyoogle-ads/build.gradle.kts
@@ -99,7 +99,7 @@ afterEvaluate {
                 artifactId = "whygoogle-ads"
 
                 // Version Library Name (Example : "1.0.0")
-                version = "1.4.5"
+                version = "1.4.6"
             }
         }
     }
