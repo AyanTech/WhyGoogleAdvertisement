543db0ed2f8db5eddcef4d507e4c77fdb00c389a
Zahra Hosseini <s.z.hosseini9473@gmail.com>
2024-01-08 10:10:50 +0330

    Increase version name

--- whyoogle-ads/build.gradle.kts
+++ whyoogle-ads/build.gradle.kts
@@ -101,7 +101,7 @@ afterEvaluate {
                 artifactId = "whygoogle-ads"
 
                 // Version Library Name (Example : "1.0.0")
-                version = "2.1.1"
+                version = "2.1.2"
             }
         }
     }
